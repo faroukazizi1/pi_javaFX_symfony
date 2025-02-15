d14ddb3d216ae06e6ad3eb2a50943ad4a44c29c3
faroukazizi1 <azizi.farouk@esprit.tn>
2025-02-15 13:06:12 +0100

    First Commit Prepration du dossier pour l'equipe

---
+++ src/main/java/Util/DBconnection.java
@@ -0,0 +1,37 @@
+package Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+
+    private String url = "jdbc:mysql://localhost:3306/management_user";
+    private String user = "root";
+    private String password = "";
+    private Connection conn;
+    private static DBconnection instance;
+
+    public static DBconnection getInstance() {
+        if (instance == null) {
+            instance = new DBconnection();
+        }
+        return instance;
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    private DBconnection() {
+        try {
+            this.conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection established");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
