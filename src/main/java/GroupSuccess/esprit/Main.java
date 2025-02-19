fc3bd3c41c48618ef9b9d9c4c351ddb23c475479
faroukazizi1 <azizi.farouk@esprit.tn>
2025-02-19 18:54:29 +0100

    Rendre le travail propre pour merge avec main

--- src/main/java/GroupSuccess/esprit/Main.java
+++ src/main/java/GroupSuccess/esprit/Main.java
@@ -1,19 +1,5 @@
 package GroupSuccess.esprit;
 
-import Service.IService;
-import Util.DBconnection;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import Util.DBconnection;
-
-import Model.promotion;
-import Model.user;
-
-import Service.promotionService;
-import Service.userService;
-import Service.IService;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
