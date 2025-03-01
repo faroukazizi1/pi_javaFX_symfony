40a65ed08ecb9e7320d6997b495c7801a0876028
faroukazizi1 <azizi.farouk@esprit.tn>
2025-03-01 14:28:44 +0100

    more cleaning before merging with main

--- src/main/java/GroupSuccess/esprit/Main.java
+++ src/main/java/GroupSuccess/esprit/Main.java
@@ -2,14 +2,9 @@ package GroupSuccess.esprit;
 
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class MainApp extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
